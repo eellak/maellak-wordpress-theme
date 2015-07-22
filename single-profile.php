0f071fad0a08f9466c605c0d6e6a52b1d1c641d0
Achilleas Pipinellis <axilleas@axilleas.me>
2015-07-22 13:04:22 +0300

    Fix linkedin url.

--- single-profile.php
+++ single-profile.php
@@ -3,7 +3,7 @@
 * Αρχείο Προβολής Στοιχείων profile
 *
 * @licence GPL
-* 
+*
 * Project URL http://ma.ellak.gr
 */
 	get_header();
@@ -13,7 +13,7 @@
 			<p><a href="<?php echo get_permalink(get_option_tree('ma_ellak_list_profiles'))?>">ΠΙΣΩ ΣΤΗ ΛΙΣΤΑ</a></p>
 		</div>
 	</div>
-	
+
 	<?php
 		if (have_posts()) : while (have_posts()) : the_post();
 			$post_id=get_the_ID();
@@ -25,24 +25,24 @@
 			$logo=$meta['_ma_profile_logo'][0];
 			$property=$meta['_ma_profile_property'][0];
 			$experience=$meta['_ma_ellak_profile_experience'][0];
-			
+
 
 			$scomment_list = '';
-			
-			
+
+
 			$args = array(
 					'status' => 'approve',
 					'post_id' =>  $post->ID , // use post_id, not post_ID
 			);
-				
+
 			$scomments = get_comments($args);
 			if ( count($scomments ) > 0 ) {
 				foreach ($scomments as $comment) {
 					$comment_content = '<li id="comment-'.$comment->comment_ID.'">';
-					
+
 					//get_avatar( $comment, 32 );
 					$comment_content .= '<div class="comment_content">' . $comment->comment_content.'</div>';
-					
+
 					if(!empty($comment->comment_author))
 						$comment_content .= '<div class="comment_author"><a href="'.$comment->comment_author_url.'">'.$comment->comment_author.'</a> @ ';
 					else {
@@ -50,13 +50,13 @@
 						$member_id = bp_core_get_userid( $user->user_login );
 						$comment_content .= '<div class="comment_author"><a href="'.bp_core_get_user_domain( $member_id ).'">'.bp_core_get_user_displayname( $member_id ).'</a> @ ';
 					}
-					
+
 					$comment_content .= mysql2date('j M Y',$comment->comment_date);
 					$comment_content .= '</div></li>';
 					$scomment_list .= $comment_content;
 				}
 			}
-			
+
 	?>
 	<div class="row-fluid event">
 		<div class="cols">
@@ -75,7 +75,7 @@
 					<?php
 					if ($property!='')
 						echo"<h3>". $property ."</h3>";
-				
+
 					echo"<div id='profil'><p class='social'>";
 					if ($meta['_ma_profile_facebook'][0]!='')
 						print_social_image('facebook', $meta['_ma_profile_facebook'][0]);
@@ -87,13 +87,13 @@
 						print_social_image('email', $meta['_ma_profile_email'][0]);
 					?>
 				    </div>
-				<?php 
+				<?php
 					$views = ma_ellak_show_statistics($post_id, 'profile');
 					if (!isset($views))
 						$views=0;
 				?>
 				<br/>
-				
+
 				<?php the_ratings();?>
 				<div class="profil-views">
 				<span class="magenta"><i class="icon-eye-open"></i> <?php echo $views; ?> </span>
@@ -105,8 +105,8 @@
 				<p>&nbsp;</p>
 				<p>
 					<?php
-					the_tags('<span class="profil-tags">','</span><span class="profil-tags">','</span>&nbsp;<br/><br/>'); 					
-		
+					the_tags('<span class="profil-tags">','</span><span class="profil-tags">','</span>&nbsp;<br/><br/>');
+
 					?>
 				</p>
 				<?php
@@ -117,14 +117,14 @@
 			</div><!-- span8 text col -->
 		</div><!-- cols -->
 	</div><!-- row-fluid event -->
-	</div>  
-	
+	</div>
+
 	<!-- TABS  -->
 	 <div class="back-purple">
         <div class="container">
           <div class="row-fluid">
             <ul class="nav nav-tabs span12">
-					<?php 
+					<?php
 					if (isset($_GET['comment']) && $_GET['comment']=='comment')
 						echo"<li>";
 					else
@@ -134,22 +134,22 @@
 					</li>
 					<li>
 						<a href="#tab-1" data-toggle="tab"><?php echo  __('ΕΡΓΑΣΙΕΣ','ma-ellak');?></a>
-					</li>					
+					</li>
 					<li><a href="#tab-2"><?php echo __('ΥΠΗΡΕΣΙΕΣ','ma-ellak')?></a></li>
 					<li <?php if(isset($_GET['comment'])) echo ' class="active"';?>><a href="#tab-3" ><?php echo __('ΣΧΟΛΙΑ','ma-ellak')?></a></li>
-							
+
             </ul>
           </div>
         </div>
       </div>
-<!--  end of basic tabs -->      
-      
+<!--  end of basic tabs -->
+
 <!--  start tabs content -->
       <div class="back-gray" id="buddypress">
         <div class="container">
           <div class="row-fluid">
             <div class="tab-content span12">
-            
+
             <!-- tab0 -->
              <div id="tab-0" class="tab-pane<?php if(!isset($_GET['comment'])) echo ' active'; ?>">
 				<div class="row-fluid">
@@ -185,10 +185,10 @@
 								else if ($status=='done')
 									$st='Ολοκληρωμένη';
 								echo __($title, 'ma_ellak') ." <strong class=\"magenta\">". $st ."</strong><br>";
-								
+
 							}
 						}else echo __('Δεν έχει καταχωρήσει Εργασίες','ma-ellak');
-				?>					
+				?>
 				</div>
 				</div>
               </div>
@@ -207,7 +207,7 @@
 									echo"</strong>";
 									echo"<BR>";
 								}
-								
+
 								$packages=wp_get_post_terms($post_id, 'package');
 								if (count($packages)>0){
 									echo "<span class='magenta'>". __("Πακέτα λογισμικού: ", "ma_ellak")."</span>";
@@ -263,7 +263,7 @@
 							<?php if ( get_option('comment_registration') && !$user_ID ) : ?>
 								<p><?php printf(__('You must be <a href="%s">logged in</a> to post a comment.'), get_option('siteurl')."/wp-login.php?redirect_to=".urlencode(get_permalink()));?></p>
 							<?php else : ?>
-							
+
 								<form action="<?php echo get_option('siteurl'); ?>/wp-comments-post.php" method="post" id="commentform">
 								<?php if ( $user_ID ) : ?>
 									<p><?php printf(__('Συνδεδεμένος ως %s.'), '<a href="'.get_option('siteurl').'/wp-admin/profile.php">'.$user_identity.'</a>'); ?> <a href="<?php echo get_option('siteurl'); ?>/wp-login.php?action=logout" title="<?php _e('Αποσύνδεση από το λογαριασμό') ?>"><?php _e('Αποσύνδεση &raquo;'); ?></a></p>
@@ -297,15 +297,15 @@
 					</div>
 				</div>
               </div>
-			  
+
             <!-- tab3 -->
             </div><!-- tab-content span12 -->
 		</div><!-- row-fluid -->
 	</div><!-- container  -->
 	<!-- back-gray -->
 <!--  end tabs content -->
-	
-	
+
+
 	<?php
   endwhile; else: ?>
 		<p>Sorry, no posts matched your criteria.</p>
@@ -313,14 +313,14 @@
 <?php
   endif;
   echo social_output();
-  
+
   get_footer();
-  
+
 function print_social_image($type, $url){
 	if ($type=='email')
 		echo "<a href=\"mailto:$url\"><img src=\"". get_template_directory_uri() ."/images/".$type.".jpg\" alt=\"". $email ."\" width=\"20\" height=\"20\"/></a>";
 	else{
-		
+
 		if($type=='facebook'){
 			if(!strstr('facebook',$url))
 				$url = 'http://www.facebook.com/'.$url;
@@ -331,11 +331,11 @@ function print_social_image($type, $url){
 		}
 		if($type=='linkedin'){
 			if(!strstr('linkedin',$url))
-				$url = 'http://www.linkedin.com/profile/view/?'.urlencode("id=".$url);
+				$url = 'https://www.linkedin.com/in/'.urlencode("id=".$url);
 		}
-	
+
 		echo "<a href=". $url ." target='_blank'><img src=\"". get_template_directory_uri() ."/images/". $type.".jpg\" alt=\"". $type ."\" title=\"". $type ."\" width=\"20\" height=\"20\"/></a>";
 	}
 }
-  
+
 ?>
