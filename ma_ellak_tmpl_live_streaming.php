85d17d4d1d8d77fb0c3aa85ebe8096eb2f84593d
Achilleas Pipinellis <axilleas@axilleas.me>
2015-07-28 12:45:55 +0300

    Add a more sensible description when the program is not yet uploaded.

--- ma_ellak_tmpl_live_streaming.php
+++ ma_ellak_tmpl_live_streaming.php
@@ -9,21 +9,21 @@
 ma_ellak_streaming_redirect();
   get_header();
   ?>
-    <?php 
+    <?php
   if (have_posts()) : while (have_posts()) : the_post();
   $ProgramPostId =get_option_tree('ma_ellak_view_event_program_option_id');
   $ParticipationFormPostId = get_option_tree('ma_ellak_view_event_option_id');
   $cid = $_GET['eventId'];
   $postdata = get_post($cid);
   $meta = get_post_meta($cid);
-  
+
   $start = $meta['_ma_event_startdate_timestamp'][0]?strtotime($meta['_ma_event_startdate_timestamp'][0]):'';
   $starttime = $meta['_ma_event_startdate_time'][0]?$meta['_ma_event_startdate_time'][0]:'';
-  
+
   $startd = date(MA_DATE_FORMAT,$start);
   $endd = $meta['_ma_event_enddate_timestamp'][0]?date(MA_DATE_FORMAT,strtotime($meta['_ma_event_enddate_timestamp'][0])):'';
   $endtime = $meta['_ma_event_enddate_time'][0]?$meta['_ma_event_enddate_time'][0]:'';
-  
+
   $SdateD = date('d',$start);
   $SdateM = date('m',$start);
   $event_type = $meta ['_ma_events_type'][0];
@@ -35,7 +35,7 @@ ma_ellak_streaming_redirect();
   $currenttime= strtotime(date(MA_DATE_FORMAT));
   $Urls = get_post_meta($cid,'eventslive');
   ?>
-  
+
   <div class="row-fluid filters">
           <div class="span6">
             <p><a href="<?php echo get_permalink(get_option_tree('ma_ellak_list_event_option_id'))?>">ΠΙΣΩ ΣΤΗ ΛΙΣΤΑ</a></p>
@@ -45,8 +45,8 @@ ma_ellak_streaming_redirect();
 		  	<div class="cols">
 		  		<div class="span8 offset2 col">
 		  			<div class="">
-					  
-				    <?php 
+
+				    <?php
 					// FOTIS for Proof of concept: USE ONLY THE XXXXXXXXXXXXXXX from the youtube.com/watch?v=XXXXXXXXXXXXX
 				  //  ma_ellak_print_streaming ($cid);
 					//echo ' <iframe width="100%" height="600px"  src="'.$Urls[0][0][_ma_ellak_event_url].'" frameborder="0" allowfullscreen></iframe>';
@@ -55,33 +55,33 @@ ma_ellak_streaming_redirect();
 					</div>
 		 		</div><!-- span4 col -->
 		 		<div class="span7 text col">
-					  <h3><a href="<?php echo $postdata->guid; ?>" rel="bookmark" 
+					  <h3><a href="<?php echo $postdata->guid; ?>" rel="bookmark"
 				  	title="<?php the_title_attribute($cid);?>" class="btn btn-large btn-link"><?php echo $postdata->post_title; ?></a>
 				  	<a href="?ical&eid=<?php echo$cid; ?>" target="_blank"><img src="<?php bloginfo('template_directory'); ?>/images/btn_ical.png" width="24" height="15" alt="ical"/></a></h3>
 				  	  <p  class="meta purple">
 					  <?php get_event_type_label($event_type);?>
-					  <?php ma_ellak_print_unit_title($cid); ?> 
+					  <?php ma_ellak_print_unit_title($cid); ?>
 					  <?php echo ma_ellak_print_thema($cid,'thema');?>
-					  </strong> 
+					  </strong>
 					  <?php echo $startd ." ". $starttime; if($endd) echo" - ". $endd ." ". $endtime;?></p>
 					  <p  class="meta purple">
 					  <?php if($place){?>
-					  <?php echo __( 'ΣΤΟ', 'ma-ellak' );?>  
-					  <strong class="magenta"><?php echo $place?></strong> 
+					  <?php echo __( 'ΣΤΟ', 'ma-ellak' );?>
+					  <strong class="magenta"><?php echo $place?></strong>
 					  <?php }?>
 					  <?php if($address)?>
 					  <strong ><?php echo $address;?></strong></p>
-					 
+
 					  <?php  the_excerpt_max_charlength(240);?>
-					  
-					  
+
+
 					  <p>&nbsp;</p>
-					
+
 				</div><!-- span8 text col -->
 		  	</div><!-- cols -->
 	  </div><!-- row-fluid event -->
-	 
-	  
+
+
   <div class="back-purple">
   	<div class="container">
   		<div class="row-fluid">
@@ -102,24 +102,24 @@ ma_ellak_streaming_redirect();
           <div class="row-fluid">
            <div class="tab-content span8 offset1">
               <div id="tab-1" class="tab-pane active">
-              	<?php 
+              	<?php
               	echo  apply_filters('the_content', $postdata->post_content);
-              	 
+
               	?>
               </div>
-              
+
               <?php if(strlen($program)>6){?>
               <div id="tab-2" class="tab-pane">
 
-				  <?php 
-					echo  apply_filters('the_content', 'hello');
-					
+				  <?php
+					echo  apply_filters('the_content', 'Το πρόγραμμα δεν έχει προστεθεί ακόμα.');
+
 					if($meta['_ma_event_program_pdf'][0]){
 						echo  "<a href='".$meta['_ma_event_program_pdf'][0]."'>";
 						echo  __('Κατεβάστε το αρχείο','ma-ellak');
 						echo"</a>";
 					}
-		
+
 				  ?>
 
               </div>
@@ -128,9 +128,9 @@ ma_ellak_streaming_redirect();
            </div>
          </div>
        </div>
-	 
+
 	 <?php echo social_output();?>
-     
+
 	<?php
   endwhile; else: ?>
 
@@ -138,7 +138,7 @@ ma_ellak_streaming_redirect();
 
 <?php
   endif;
-  
+
   get_footer();
 
 ?>
